09717f6b1d7877ccb49e449087f3d55c97a259d6
Inspecting <96089121+Inspecting@users.noreply.github.com>
2025-07-25 23:22:49 -0500

    Create navbar.js

---
+++ shared/navbar.js
@@ -0,0 +1,14 @@
+document.querySelectorAll('nav button').forEach(btn => {
+  btn.onclick = () => {
+    const page = btn.dataset.page;
+    if (page === 'home') {
+      window.location.href = '/bilm.github.io/home/';
+    } else if (page === 'movies') {
+      window.location.href = '/bilm.github.io/movies/';
+    } else if (page === 'tv') {
+      window.location.href = '/bilm.github.io/tv-shows/';
+    } else if (page === 'settings') {
+      window.location.href = '/bilm.github.io/settings/';
+    }
+  };
+});
